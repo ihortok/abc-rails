3492ec405a1dfb883c5a1fc086f6cfb755c1b6e2
ihortok <itokaryk@gmail.com>
2026-01-18 14:40:59 +0100

    display image and color for words

--- lib/database_seeds/ukrainian_alphabet_seed.rb
+++ lib/database_seeds/ukrainian_alphabet_seed.rb
@@ -1,67 +1,177 @@
 module DatabaseSeeds
   class UkrainianAlphabetSeed
     LETTERS = %w[а б в г ґ д е є ж з и і ї й к л м н о п р с т у ф х ц ч ш щ ь ю я].freeze
-    COLORS = {
-      "Червоний" => "Red",
-      "Помаранчевий" => "Orange",
-      "Жовтий" => "Yellow",
-      "Зелений" => "Green",
-      "Блакитний" => "Sky Blue",
-      "Синій" => "Blue",
-      "Фіолетовий" => "Purple",
-      "Чорний" => "Black",
-      "Білий" => "White",
-      "Коричневий" => "Brown",
-      "Сірий" => "Gray",
-      "Рожевий" => "Pink"
-    }.freeze
     WORD_PICTURES_DIR = File.join(__dir__, "files", "word_pictures")
-    WORD_IMAGE_FILENAMES = {
-      "Ананас" => "ananas.png",
-      "Антилопа" => "antelope.png",
-      "Апельсин" => "orange.png",
-      "Банан" => "banana.png",
-      "Бабуся" => "grandma.png",
-      "Бджола" => "bee.png",
-      "Ведмідь" => "bear.png",
-      "Ваза" => "vase.png",
-      "Вітер" => "wind.png",
-      "Гарбуз" => "pumpkin.png",
-      "Гусак" => "goose.png",
-      "Ґудзик" => "button.png",
-      "Ґанок" => "porch.png",
-      "Ґрунт" => "soil.png",
-      "Дерево" => "tree.png",
-      "Дім" => "house.png",
-      "Дорога" => "road.png",
-      "Екран" => "screen.png",
-      "Електрика" => "electricity.png",
-      "Енергія" => "energy.png",
-      "Єнот" => "raccoon.png",
-      "Єдність" => "unity.png",
-      "Єдиноріг" => "unicorn.png",
-      "Жирафа" => "giraffe.png",
-      "Жук" => "bug.png",
-      "Життя" => "life.png",
-      "Зебра" => "zebra.png",
-      "Зірка" => "star.png",
-      "Зима" => "winter.png",
-      "Іграшка" => "toy.png",
-      "Інструмент" => "tools.png",
-      "Ідея" => "idea.png",
-      "Їжак" => "hedgehog.png",
-      "Їжа" => "food.png",
-      "Їдальня" => "dining_room.png",
-      "Йогурт" => "yogurt.png",
-      "Йога" => "yoga.png",
-      "Йод" => "iodine.png",
-      "Кіт" => "cat.png",
-      "Книга" => "book.png",
-      "Квітка" => "flower.PNG",
-      "Лев" => "lion.png",
-      "Лампа" => "lamp.png",
-      "Лист" => "leaf.png",
-      "Миша" => "mouse.png"
+    WORD_SEEDS = {
+      "а" => [
+        { content: "Апельсин", image: "orange.png" },
+        { content: "Ананас", image: "ananas.png" },
+        { content: "Антилопа", image: "antelope.png" }
+      ],
+      "б" => [
+        { content: "Банан", image: "banana.png" },
+        { content: "Бабуся", image: "grandma.png" },
+        { content: "Бджола", image: "bee.png" },
+        { content: "Білий", color: "White" },
+        { content: "Блакитний", color: "Sky Blue" }
+      ],
+      "в" => [
+        { content: "Ведмідь", image: "bear.png" },
+        { content: "Ваза", image: "vase.png" },
+        { content: "Вітер", image: "wind.png" }
+      ],
+      "г" => [
+        { content: "Гарбуз", image: "pumpkin.png" },
+        { content: "Година" },
+        { content: "Гусак", image: "goose.png" }
+      ],
+      "ґ" => [
+        { content: "Ґудзик", image: "button.png" },
+        { content: "Ґанок", image: "porch.png" },
+        { content: "Ґрунт", image: "soil.png" }
+      ],
+      "д" => [
+        { content: "Дерево", image: "tree.png" },
+        { content: "Дім", image: "house.png" },
+        { content: "Дорога", image: "road.png" }
+      ],
+      "е" => [
+        { content: "Екран", image: "screen.png" },
+        { content: "Електрика", image: "electricity.png" },
+        { content: "Енергія", image: "energy.png" }
+      ],
+      "є" => [
+        { content: "Єнот", image: "raccoon.png" },
+        { content: "Єдність", image: "unity.png" },
+        { content: "Єдиноріг", image: "unicorn.png" }
+      ],
+      "ж" => [
+        { content: "Жирафа", image: "giraffe.png" },
+        { content: "Жук", image: "bug.png" },
+        { content: "Життя", image: "life.png" },
+        { content: "Жовтий", color: "Yellow" }
+      ],
+      "з" => [
+        { content: "Зебра", image: "zebra.png" },
+        { content: "Зірка", image: "star.png" },
+        { content: "Зима", image: "winter.png" },
+        { content: "Зелений", color: "Green" }
+      ],
+      "и" => [], # No common Ukrainian words start with "и"
+      "і" => [
+        { content: "Іграшка", image: "toy.png" },
+        { content: "Інструмент", image: "tools.png" },
+        { content: "Ідея", image: "idea.png" }
+      ],
+      "ї" => [
+        { content: "Їжак", image: "hedgehog.png" },
+        { content: "Їжа", image: "food.png" },
+        { content: "Їдальня", image: "dining_room.png" }
+      ],
+      "й" => [
+        { content: "Йогурт", image: "yogurt.png" },
+        { content: "Йога", image: "yoga.png" },
+        { content: "Йод", image: "iodine.png" }
+      ],
+      "к" => [
+        { content: "Кіт", image: "cat.png" },
+        { content: "Книга", image: "book.png" },
+        { content: "Квітка", image: "flower.PNG" },
+        { content: "Коричневий", color: "Brown" }
+      ],
+      "л" => [
+        { content: "Лев", image: "lion.png" },
+        { content: "Лампа", image: "lamp.png" },
+        { content: "Лист", image: "leaf.png" }
+      ],
+      "м" => [
+        { content: "Миша", image: "mouse.png" },
+        { content: "Море" },
+        { content: "Місто" }
+      ],
+      "н" => [
+        { content: "Ніс" },
+        { content: "Нота" },
+        { content: "Небо" }
+      ],
+      "о" => [
+        { content: "Олівець" },
+        { content: "Око" },
+        { content: "Острів" }
+      ],
+      "п" => [
+        { content: "Пес" },
+        { content: "Птах" },
+        { content: "Пляж" },
+        { content: "Помаранчевий", color: "Orange" }
+      ],
+      "р" => [
+        { content: "Ракета" },
+        { content: "Риба" },
+        { content: "Рука" },
+        { content: "Рожевий", color: "Pink" }
+      ],
+      "с" => [
+        { content: "Сонце" },
+        { content: "Собака" },
+        { content: "Стіл" },
+        { content: "Синій", color: "Blue" },
+        { content: "Сірий", color: "Gray" }
+      ],
+      "т" => [
+        { content: "Тигр" },
+        { content: "Трава" },
+        { content: "Торт" }
+      ],
+      "у" => [
+        { content: "Усмішка" },
+        { content: "Урок" },
+        { content: "Ураган" }
+      ],
+      "ф" => [
+        { content: "Фрукт" },
+        { content: "Фонтан" },
+        { content: "Фіалка" },
+        { content: "Фіолетовий", color: "Purple" }
+      ],
+      "х" => [
+        { content: "Хатина" },
+        { content: "Хліб" },
+        { content: "Хмара" }
+      ],
+      "ц" => [
+        { content: "Цукор" },
+        { content: "Цвіт" },
+        { content: "Цирк" }
+      ],
+      "ч" => [
+        { content: "Чайка" },
+        { content: "Час" },
+        { content: "Човен" },
+        { content: "Червоний", color: "Red" },
+        { content: "Чорний", color: "Black" }
+      ],
+      "ш" => [
+        { content: "Шапка" },
+        { content: "Школа" },
+        { content: "Шоколад" }
+      ],
+      "щ" => [
+        { content: "Щука" },
+        { content: "Щастя" },
+        { content: "Щітка" }
+      ],
+      "ь" => [], # No words start with the soft sign
+      "ю" => [
+        { content: "Юрба" },
+        { content: "Юнак" },
+        { content: "Ювелір" }
+      ],
+      "я" => [
+        { content: "Яблуко" },
+        { content: "Ягода" },
+        { content: "Яхта" }
+      ]
     }.freeze
 
 
@@ -74,54 +184,16 @@ module DatabaseSeeds
         alphabet.letters.find_or_create_by(character: letter, position: index + 1)
       end
 
-      # Create some words for each letter
-      words = {
-        "а" => [ "Апельсин", "Ананас", "Антилопа" ],
-        "б" => [ "Банан", "Бабуся", "Бджола", "Білий", "Блакитний" ],
-        "в" => [ "Ведмідь", "Ваза", "Вітер" ],
-        "г" => [ "Гарбуз", "Година", "Гусак" ],
-        "ґ" => [ "Ґудзик", "Ґанок", "Ґрунт" ],
-        "д" => [ "Дерево", "Дім", "Дорога" ],
-        "е" => [ "Екран", "Електрика", "Енергія" ],
-        "є" => [ "Єнот", "Єдність", "Єдиноріг" ],
-        "ж" => [ "Жирафа", "Жук", "Життя", "Жовтий" ],
-        "з" => [ "Зебра", "Зірка", "Зима", "Зелений" ],
-        "и" => [], # No common Ukrainian words start with "и"
-        "і" => [ "Іграшка", "Інструмент", "Ідея" ],
-        "ї" => [ "Їжак", "Їжа", "Їдальня" ],
-        "й" => [ "Йогурт", "Йога", "Йод" ],
-        "к" => [ "Кіт", "Книга", "Квітка", "Коричневий" ],
-        "л" => [ "Лев", "Лампа", "Лист" ],
-        "м" => [ "Миша", "Море", "Місто" ],
-        "н" => [ "Ніс", "Нота", "Небо" ],
-        "о" => [ "Олівець", "Око", "Острів" ],
-        "п" => [ "Пес", "Птах", "Пляж", "Помаранчевий" ],
-        "р" => [ "Ракета", "Риба", "Рука", "Рожевий" ],
-        "с" => [ "Сонце", "Собака", "Стіл", "Синій", "Сірий" ],
-        "т" => [ "Тигр", "Трава", "Торт" ],
-        "у" => [ "Усмішка", "Урок", "Ураган" ],
-        "ф" => [ "Фрукт", "Фонтан", "Фіалка", "Фіолетовий" ],
-        "х" => [ "Хатина", "Хліб", "Хмара" ],
-        "ц" => [ "Цукор", "Цвіт", "Цирк" ],
-        "ч" => [ "Чайка", "Час", "Човен", "Червоний", "Чорний" ],
-        "ш" => [ "Шапка", "Школа", "Шоколад" ],
-        "щ" => [ "Щука", "Щастя", "Щітка" ],
-        "ь" => [], # No words start with the soft sign
-        "ю" => [ "Юрба", "Юнак", "Ювелір" ],
-        "я" => [ "Яблуко", "Ягода", "Яхта" ]
-      }
+      WORD_SEEDS.each do |_letter, word_entries|
+        word_entries.each do |word_entry|
+          word = Word.find_or_create_by(content: word_entry[:content], alphabet: alphabet)
 
-      words.each do |letter, words|
-        words.each do |word|
-          word = Word.find_or_create_by(content: word, alphabet: alphabet)
-
-          # Associate colors if the word matches any color names
-          if COLORS.keys.include?(word.content)
-            color = Color.find_by(name: COLORS[word.content])
+          if word_entry[:color]
+            color = Color.find_by(name: word_entry[:color])
             word.colors << color if color
           end
 
-          image_filename = WORD_IMAGE_FILENAMES[word.content]
+          image_filename = word_entry[:image]
           next unless image_filename
 
           image_path = File.join(WORD_PICTURES_DIR, image_filename)
