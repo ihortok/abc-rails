5f70f69721ce91741c052ab92173603c46a261a3
ihortok <itokaryk@gmail.com>
2026-01-18 12:18:05 +0100

    unify EnglishAlphabetSeed

--- lib/database_seeds/english_alphabet_seed.rb
+++ lib/database_seeds/english_alphabet_seed.rb
@@ -5,38 +5,38 @@ module DatabaseSeeds
       alphabet = Alphabet.find_or_create_by(name: "English")
 
       # Create letters A-Z for the English alphabet
-      ("A".."Z").to_a.each do |letter|
+      ("a".."z").to_a.each do |letter|
         alphabet.letters.find_or_create_by(character: letter)
       end
 
       # Create some words for each letter
       words = {
-        "A" => [ "Apple", "Ant", "Arrow" ],
-        "B" => [ "Ball", "Bat", "Bee" ],
-        "C" => [ "Cat", "Car", "Cup" ],
-        "D" => [ "Dog", "Door", "Duck" ],
-        "E" => [ "Elephant", "Egg", "Eagle" ],
-        "F" => [ "Fish", "Fan", "Frog" ],
-        "G" => [ "Goat", "Gate", "Gum" ],
-        "H" => [ "Hat", "Horse", "House" ],
-        "I" => [ "Ice", "Igloo", "Insect" ],
-        "J" => [ "Jar", "Jam", "Jacket" ],
-        "K" => [ "Kite", "Key", "Kangaroo" ],
-        "L" => [ "Lion", "Lamp", "Leaf" ],
-        "M" => [ "Monkey", "Moon", "Map" ],
-        "N" => [ "Nest", "Net", "Nose" ],
-        "O" => [ "Owl", "Orange", "Ox" ],
-        "P" => [ "Pig", "Pen", "Pineapple" ],
-        "Q" => [ "Queen", "Quilt", "Quail" ],
-        "R" => [ "Rat", "Ring", "Rocket" ],
-        "S" => [ "Sun", "Star", "Snake" ],
-        "T" => [ "Tiger", "Tree", "Table" ],
-        "U" => [ "Umbrella", "Unicorn", "Urchin" ],
-        "V" => [ "Van", "Vase", "Violin" ],
-        "W" => [ "Wolf", "Water", "Wheel" ],
-        "X" => [ "Xylophone", "X-ray" ],
-        "Y" => [ "Yak", "Yarn", "Yacht" ],
-        "Z" => [ "Zebra", "Zoo", "Zipper" ]
+        "a" => [ "Apple", "Ant", "Arrow" ],
+        "b" => [ "Ball", "Bat", "Bee" ],
+        "c" => [ "Cat", "Car", "Cup" ],
+        "d" => [ "Dog", "Door", "Duck" ],
+        "e" => [ "Elephant", "Egg", "Eagle" ],
+        "f" => [ "Fish", "Fan", "Frog" ],
+        "g" => [ "Goat", "Gate", "Gum" ],
+        "h" => [ "Hat", "Horse", "House" ],
+        "i" => [ "Ice", "Igloo", "Insect" ],
+        "j" => [ "Jar", "Jam", "Jacket" ],
+        "k" => [ "Kite", "Key", "Kangaroo" ],
+        "l" => [ "Lion", "Lamp", "Leaf" ],
+        "m" => [ "Monkey", "Moon", "Map" ],
+        "n" => [ "Nest", "Net", "Nose" ],
+        "o" => [ "Owl", "Orange", "Ox" ],
+        "p" => [ "Pig", "Pen", "Pineapple" ],
+        "q" => [ "Queen", "Quilt", "Quail" ],
+        "r" => [ "Rat", "Ring", "Rocket" ],
+        "s" => [ "Sun", "Star", "Snake" ],
+        "t" => [ "Tiger", "Tree", "Table" ],
+        "u" => [ "Umbrella", "Unicorn", "Urchin" ],
+        "v" => [ "Van", "Vase", "Violin" ],
+        "w" => [ "Wolf", "Water", "Wheel" ],
+        "x" => [ "Xylophone", "X-ray" ],
+        "y" => [ "Yak", "Yarn", "Yacht" ],
+        "z" => [ "Zebra", "Zoo", "Zipper" ]
       }
 
       words.each do |letter, words|
